d6dbff93fc1dcb624740d6cc99149e731533d0b2
Zoltán Galáž <xgalaz00@gmail.com>
2021-05-28 11:09:27 +0200

    Added data configuration

---
+++ app/configuration/__init__.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+
+# Prepare the configuration path
+configuration_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def load_configuration(configuration_filename):
+    """Loads the configuration from the specified configuration file name"""
+
+    # Prepare the api configuration file path
+    path = os.path.join(configuration_path, configuration_filename)
+
+    # Read the configuration
+    if path and os.path.isfile(path) and os.access(path, os.R_OK):
+        with open(path, 'r') as f:
+            return json.load(f)
