890d235a79cd7479ad9495060983ddcf84a9d351
Zoltán Galáž <xgalaz00@gmail.com>
2021-06-12 12:05:29 +0200

    Added PredictorApiClient

---
+++ predictor/client.py
@@ -0,0 +1,76 @@
+import requests
+from django.conf import settings
+
+
+class PredictorApiClient(object):
+    """Class implementing the Predictor API client"""
+
+    # Define the HTTP error codes to be handled by the re-logging
+    logging_required_errors = [401, 422]
+
+    # Define the predictor address
+    address = getattr(settings, 'PREDICTOR_API_URL')
+
+    def __init__(self, user):
+        self.user = user
+
+    def sign_up(self):
+        """Signs-up a user in the predictor API"""
+        return requests.post(f'{self.address}/signup', json=self.user.get_predictor_authentication_data())
+
+    def log_in(self):
+        """Logs-in a user in the predictor API"""
+        return requests.post(f'{self.address}/login', json=self.user.get_predictor_authentication_data())
+
+    def predict(self, data=None, model=None):
+        """Predicts the LBD probability via the predictor API"""
+
+        # Prepare the data to be sent via the API
+        data = {
+            'features': {'data': data},
+            'model': model
+        }
+
+        # Prepare the headers to be sent via the API
+        headers = self.user.get_predictor_authorization_data()
+
+        # Run the predictor
+        return requests.post(f'{self.address}/predict', json=data, headers=headers, verify=True)
+
+
+def predict_lbd_probability(user, data, model):
+    """
+    Predicts the LBD probability via the predictor API.
+
+    :param user: user model instance
+    :type user: User instance
+    :param data: data to be used for the prediction
+    :type data: data supported by the API
+    :param model: model identifier to be used
+    :type model: str
+    :return: predicted LBD probability
+    :rtype: float
+    """
+
+    # Prepare the predictor API client using the provided user instance
+    predictor = PredictorApiClient(user)
+
+    # Predict the LBD probability via the predictor API using the provided data and model identifier
+    response = predictor.predict(data=data, model=model)
+
+    # Handle the authorization token errors
+    if not response.ok:
+        if response.status_code in predictor.logging_required_errors:
+            user.predictor_authorization_token = predictor.log_in().json().get('token')
+            user.save()
+            response = predictor.predict(data=data, model=model)
+
+    if not response.ok:
+        return None
+
+    # Extract the LBD probability
+    probability = response.json().get('proba')
+    probability = round(float(probability) * 100, 2) if probability is not None else None
+
+    # Return the LBD probability
+    return probability
